75e06ec65d6fa7958e706a5b8026c25813bd613a
34bitKachan <kachan.maksim1@yandex.ru>
2025-03-02 02:28:38 +0300

    Делаю 3 и 4 уровень

--- Assets/Scripts/Other/Buttom.cs
+++ Assets/Scripts/Other/Buttom.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class Buttom : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private ElectricalSystem[] _electricalSystems;
+    [SerializeField] private float _scaleActivMin = 1f;
+    [SerializeField] private float _scaleActivMax = 1.5f;
+    [SerializeField] private float _distanceDown;
+    [SerializeField] private float _timeDown = 0.01f;
+    private Vector3 _startPos;
+    [SerializeField] private bool _activeButton = false;
     void Start()
     {
-        
+        _startPos = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(_activeButton)
+        {
+            transform.position = Vector3.Lerp(transform.position, _startPos - Vector3.up * _distanceDown, _timeDown);
+        }
+        else if(!_activeButton)
+        {
+            transform.position = Vector3.Lerp(transform.position, _startPos, _timeDown);
+        }
+    }
+
+    private void OnTriggerStay(UnityEngine.Collider other)
+    {
+        GameObject gam = other.gameObject;
+        if (gam.tag == "Obj" && gam.transform.localScale.y > _scaleActivMin && gam.transform.localScale.y < _scaleActivMax)
+        {
+            foreach (ElectricalSystem el in _electricalSystems)
+                el.Active = true;
+            _activeButton = true;
+        }
+        else
+        {
+            foreach (ElectricalSystem el in _electricalSystems)
+                el.Active = false;
+            _activeButton = false;
+        }
+    }
+
+    private void OnTriggerExit(UnityEngine.Collider other)
+    {
+        GameObject gam = other.gameObject;
+        if (gam.tag == "Obj")
+        {
+            foreach (ElectricalSystem el in _electricalSystems)
+                el.Active = false;
+            _activeButton = false;
+        }
     }
 }
